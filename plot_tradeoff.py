f967c14333770ab7b695e9caa802ca02476422df
liam-blaschka <liam.blaschka@student.adelaide.edu.au>
2025-11-04 19:20:04 +1030

    trade-off plots

---
+++ plot_tradeoff.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+pids = [2100, 2101, 2102, 2103, 2200, 2201, 2202, 2203, 2300, 2301, 2302]
+
+for pid in pids:
+
+    df = pd.read_csv(f"ex2-10_000/{pid}_GSEMO_tradeoff_first_run.csv")
+
+    x = df["size"]
+    y = df["value_f"]
+
+    # Plot
+    plt.plot(x, y, marker='o', label="Pareto front")
+    plt.scatter(x, y)
+
+    plt.xlabel("Size")
+    plt.ylabel("Function value")
+    plt.title(f"F{pid} GSEMO First Run Trade-off (10,000 budget)")
+    plt.grid(True, linestyle="--", alpha=0.5)
+    plt.legend()
+    plt.tight_layout()
+
+    plt.savefig(f"F{pid}_GSEMO_trade-off", dpi=150)
+
+    # plt.show()
+    
+    plt.close()
+    
